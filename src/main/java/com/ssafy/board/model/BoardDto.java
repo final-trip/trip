d78a8a9832eb5840751a2b7b3982d8e3f1ab3643
hanjs0625 <hanjs0625@gmail.com>
2023-11-01 09:08:22 +0900

    1

---
+++ src/main/java/com/ssafy/board/model/BoardDto.java
@@ -0,0 +1,29 @@
+package com.ssafy.board.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class BoardDto {
+
+	private int articleNo;
+	private String userId;
+	private String userName;
+	private String subject;
+	private String content;
+	private int hit;
+	private String registerTime;
+	private List<FileInfoDto> fileInfos;
+ 
+}
