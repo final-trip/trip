fe6ab4315e67fe11e47382d6fb677e589593321b
junwon <junwon1131@naver.com>
2023-11-03 10:53:59 +0900

    api operation adding

--- src/main/java/com/ssafy/mountain/controller/Mountaincontroller.java
+++ src/main/java/com/ssafy/mountain/controller/Mountaincontroller.java
@@ -1,5 +1,6 @@
-package com.ssafy.mountain.model.controller;
+package com.ssafy.mountain.controller;
 
+ 
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +18,14 @@ import com.ssafy.member.model.MemberDto;
 import com.ssafy.mountain.model.MountainDto;
 import com.ssafy.mountain.model.service.MountainService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @Controller
+@Api(tags = { "마운틴 컨트롤러  API V1" })
+
 @RequestMapping("/mountain")
 public class Mountaincontroller {
 
@@ -28,6 +36,10 @@ public class Mountaincontroller {
 		this.mountainservice = mountainservice;
 	}
 
+	@ApiOperation(value = "산 추가", notes = "산의 정보를 추가한다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Mountain added successfully"),
+			@ApiResponse(code = 404, message = "Failed to add mountain"),
+			@ApiResponse(code = 500, message = "서버에러!!") })
 	@PostMapping("/add")
 	public ResponseEntity<String> addMountain(@RequestBody MountainDto mountainDto) {
 		try {
@@ -38,6 +50,10 @@ public class Mountaincontroller {
 		}
 	}
 
+	@ApiOperation(value = "정복한 산 추가", notes = "회원이 정복한 산 정보를 추가한다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Mountain added successfully"),
+			@ApiResponse(code = 404, message = "Failed to add mountain"),
+			@ApiResponse(code = 500, message = "서버에러!!") })
 	@PostMapping("/add/conqueredMountain")
 	public ResponseEntity<String> AddconqueredMountain(@RequestParam("memberId") int memberId,
 			@RequestParam("mntilistno") int mntilistno) {
@@ -49,6 +65,8 @@ public class Mountaincontroller {
 		}
 	}
 
+	@ApiOperation(value = "아직 정복하지 않은 산 목록 가져오기", notes = "회원이 아직 정복하지 않은 산 목록을 가져온다.")
+
 	@GetMapping("/unconquered")
 	public ResponseEntity<List<MountainDto>> getUnconqueredMountains(@RequestParam("memberId") int memberId) {
 		try {
@@ -59,11 +77,15 @@ public class Mountaincontroller {
 		}
 	}
 
+	@ApiOperation(value = "모든 산 목록 가져오기", notes = "모든 산의 목록을 가져온다.")
+
 	@GetMapping("/all")
 	public ResponseEntity<List<MountainDto>> allmountains() throws SQLException {
 		List<MountainDto> allMountainDtos = mountainservice.allmountains();
 		return ResponseEntity.ok(allMountainDtos);
 	}
+
+	@ApiOperation(value = "아직 정복하지 않은 산을 높이 순으로 가져오기", notes = "회원이 아직 정복하지 않은 산을 높이 순으로 가져온다.")
 
 	@GetMapping("/unconquered/ascending")
 	public ResponseEntity<List<MountainDto>> getUnconqueredMountainsAscendingByHeight(
@@ -76,6 +98,8 @@ public class Mountaincontroller {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.emptyList());
 		}
 	}
+
+	@ApiOperation(value = "아직 정복하지 않은 산 중 가장 가까운 산 목록 가져오기", notes = "회원이 아직 정복하지 않은 산 중에서 가장 가까운 산 목록을 가져온다.")
 
 	@GetMapping("/unconquered/nearest")
 	public ResponseEntity<List<MountainDto>> getNearestUnconqueredMountains(@RequestParam("memberId") int memberId) {
