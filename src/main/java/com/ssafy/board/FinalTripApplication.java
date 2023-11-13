9fc3a6be22512bd8192123bb7274f839c18384ed
정준원 <junwon1131@naver.com>
2023-11-13 17:45:05 +0900

    test

---
+++ src/main/java/com/ssafy/board/FinalTripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTripApplication.class, args);
+	}
+
+}
